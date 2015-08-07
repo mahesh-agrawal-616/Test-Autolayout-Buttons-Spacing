ea7347eb1ab23294cd19b678001b12e4c847847d
Mahesh Agrawal <agrawal.mahesh313@gmail.com>
2015-08-07 11:16:02 +0530

    Initial commit

---
+++ Test/Test-Autolayout-Buttons-Spacing/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test-Autolayout-Buttons-Spacing
+//
+//  Created by Mahesh Agrawal on 8/7/15.
+//  Copyright (c) 2015 Mahesh Agrawal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Test/Test-Autolayout-Buttons-Spacing/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test-Autolayout-Buttons-Spacing
+//
+//  Created by Mahesh Agrawal on 8/7/15.
+//  Copyright (c) 2015 Mahesh Agrawal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
